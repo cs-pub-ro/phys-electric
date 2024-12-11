1d10062f4fc88d39f9cfdff34d7454d69ec76b66
robertpaulp <77567448+robertpaulp@users.noreply.github.com>
2024-12-11 13:52:47 +0200

    Updated manchester & wrote framing functionality

--- manchester_codec/receiver.h
+++ manchester_codec/receiver.h
@@ -10,10 +10,9 @@ private:
     uint8_t rx_pin;
     uint32_t baud_rate;
     uint32_t clock_period_us;
-    uart_inst_t *uart_instance = uart0;
 
 public:
-    void init(uint8_t rx_pin, uint32_t baud_rate, uint32_t clock_period_us, uart_inst_t *uart_instance);
+    void init(uint8_t rx_pin, uint32_t baud_rate, uint32_t clock_period_us);
     int receive_manchester_bit(int first_half, int second_half);
     int receive_manchester_byte();
 };
